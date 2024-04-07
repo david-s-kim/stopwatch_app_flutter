b7f8026baab7bc3850b88adeeabc7c3d3a3b57d8
David Kim <mycodingtool9@gmail.com>
2024-04-07 11:18:32 +0900

    - feat: Timer 클래스와 필요한 변수 준비

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:stopwatch_app_flutter/stop_watch_screen.dart';
 
 void main() {
-  Timer.periodic(Duration(seconds: 1), (timer) {
-    print('!!!');
-  });
+
   runApp(const MyApp());
 }
 
